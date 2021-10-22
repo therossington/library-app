1ff20bb7c6fcad6e080e75a0d1897d561cf2da02
therossington <camsymonds@outlook.com>
2021-10-22 17:12:55 +1000

    Additional JS work

--- scripts.js
+++ scripts.js
@@ -8,24 +8,52 @@ const library = document.querySelector('tbody');
 const mainPage = document.querySelector('body');
 
 
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+function Book(title, author, pages, read, entryId) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read;
+    this.entryId = entryId;
 }
 
 let titleEntry = document.querySelector('#title');
 let authorEntry = document.querySelector('#author');
 let pagesEntry = document.querySelector('#pages');
-let readEntry = document.querySelector('#read');
 
 
 function bookList() {
     const displayList = document.createElement('tr');
+    const bookTitle = document.createElement('td'); 
+    const bookAuthor = document.createElement('td');
+    const bookPages = document.createElement('td');
+    const bookRead = document.createElement('td');
+    const statusTable = document.createElement('td');
+    const deleteTable = document.createElement('td');
+    const statusChange = document.createElement('button');
+    const deleteButton = document.createElement('button');
+    
     myLibrary.forEach((myLibrary) => {
         library.appendChild(displayList);
-        displayList.innerHTML = `<td>${myLibrary.title}</td><td>${myLibrary.author}</td><td>${myLibrary.read}</td>`;
+        bookTitle.textContent = `${myLibrary.title}`;
+        bookAuthor.textContent = `${myLibrary.author}`;
+        bookPages.textContent = `${myLibrary.pages}`;
+        bookRead.textContent = `${myLibrary.read}`;
+
+        statusChange.classList.add('read-button');
+        statusChange.textContent = 'Change read status';
+        statusTable.appendChild(statusChange);
+        deleteButton.classList.add('delete-button');
+        deleteButton.textContent = 'Delete book';
+        deleteTable.appendChild(deleteButton);
+    
+        displayList.appendChild(bookTitle);
+        displayList.appendChild(bookAuthor);
+        displayList.appendChild(bookPages);
+        displayList.appendChild(bookRead);
+        displayList.appendChild(statusTable);
+        displayList.appendChild(deleteTable);
+
+        /* myLibrary.entryId = myLibrary.findIndex(x => x.title === "Cameron's book")*/
     })
 }           
 
@@ -33,8 +61,9 @@ function addBookToLibrary() {
     let title = titleEntry.value;
     let author = authorEntry.value;
     let pages = pagesEntry.value;
-    let read = true;
-    let newEntry = new Book(title, author, pages, read);
+    let read = document.querySelector('input[name="read"]:checked').value;
+    let entryId = '';
+    let newEntry = new Book(title, author, pages, read, entryId);
     myLibrary.push(newEntry);
 }
 
